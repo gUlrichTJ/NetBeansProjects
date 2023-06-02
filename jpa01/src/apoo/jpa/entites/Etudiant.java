9a80307975a347bb9785392a21e69c95837ff449
gUlrichTJ <gracianomathey@gmail.com>
2023-06-02 13:05:18 +0000

    twenty-seventh commit

--- jpa01/src/apoo/jpa/entites/Etudiant.java
+++ jpa01/src/apoo/jpa/entites/Etudiant.java
@@ -14,6 +14,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Transient;
 
 /**
@@ -22,6 +23,7 @@ import javax.persistence.Transient;
  */
 
 @Entity
+@Table(name = "etudiants")
 public class Etudiant implements Serializable {
     
     @Id     // Ceci est la clé primaire
